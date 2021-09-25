d5c10d28907a45391023594b1e171f32ddb4a5d0
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-09-26 00:25:13 +0600

    Car Show Header Added

---
+++ src/components/CarShow/Car/Car.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Car.css'
+
+const Car = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Car;
--- src/components/CarShow/CarShow.js
+++ src/components/CarShow/CarShow.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CarShow.css'
 
 const CarShow = () => {
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => {
+        fetch('cars.JSON')
+        .then(res => res.json())
+        .then(data => setCars(data));
+    }, []);
+
     return (
         <div>
-            H
+            <div className="header">
+                <h2>Car Show in Dhaka!</h2>
+                <p className="my-0">Biggest car show in 2021</p>
+                <h4>Budget: <span>10 Core BDT</span></h4>
+            </div>
         </div>
     );
 };
