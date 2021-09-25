d9f52699ec31411dad9bee79ed414ca0fad9aca9
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-09-26 01:50:51 +0600

    Car Card Design Added

--- src/components/CarShow/Car/Car.js
+++ src/components/CarShow/Car/Car.js
@@ -3,13 +3,26 @@ import './Car.css';
 
 
 const Car = (props) => {
-    
-    const {model, image, engine, power, transmission, topSpeed, price} = props.car;
+
+    const { model, image, engine, power, transmission, topSpeed, price } = props.car;
     console.log(image);
     return (
-        <div>
-            <img className="img-fluid" src={image} alt="" />
-            {model}            
+        <div className="car card">
+            <img className="img-fluid card-img-top" src={image} alt="" />
+            <div className="card-body pb-0">
+                <h5>{model}</h5>
+                <ul>
+                    <li>Engine: {engine}</li>
+                    <li>Power: {power}</li>
+                    <li>Transmission: {transmission}</li>
+                    <li>Top speed: {topSpeed}</li>
+                    <li>Price: {price}</li>
+                    <li>Engine: {engine}</li>
+                </ul>
+            </div>
+            <div className="card-footer">
+                <button className="add-car-button">Add Car</button>
+            </div>
         </div>
     );
 };
