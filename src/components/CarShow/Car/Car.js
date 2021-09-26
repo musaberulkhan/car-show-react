d0e11ca23808bf213a7717d9d75f26ae4d7fa9d6
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-09-26 17:03:30 +0600

    Car Social Icon Added

--- src/components/CarShow/Car/Car.js
+++ src/components/CarShow/Car/Car.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Car.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 
 const Car = (props) => {
 
-    const { id, model, image, engine, power, transmission, topSpeed, price } = props.car;
+    const { id, model, image, engine, power, transmission, topSpeed, price, facebook, twitter, youtube } = props.car;
 
     return (
         <div className="car card">
@@ -27,6 +28,17 @@ const Car = (props) => {
                     className="add-car-button">
                     <FontAwesomeIcon icon={faPlus} /> Add Car
                 </button>
+                <div className="social">
+                    <a href={facebook}>
+                        <FontAwesomeIcon size='2x' className="social-icon" icon={faFacebook} />
+                    </a>
+                    <a href={twitter}>
+                        <FontAwesomeIcon size='2x' className="social-icon" icon={faTwitter} />
+                    </a>
+                    <a href={youtube}>
+                        <FontAwesomeIcon size='2x' className="social-icon" icon={faYoutube} />
+                    </a>
+                </div>
             </div>
         </div>
     );
